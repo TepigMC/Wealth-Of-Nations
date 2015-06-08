11d7ef1d0fd26214e57affb6c382b923b9f83620
Andrew Archibald <andrew@robarchibald.com>
2015-06-08 08:48:44 -0700

    :smile: Initial Commit
    Started with an example from
    [here](http://www.abeautifulsite.net/java-game-programming-for-beginners/)

---
+++ src/tepigmc/wealthofnations/Game.java
@@ -0,0 +1,25 @@
+package tepigmc.wealthofnations;
+
+import javax.swing.JFrame;
+
+public class Game {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Wealth of Nations");
+    GamePanel game = new GamePanel();
+    frame.add(game);
+    frame.setSize(640, 480);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    while (true) {
+      game.moveBall();
+      game.repaint();
+      try {
+        Thread.sleep(10);
+      }
+      catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
---
+++ src/tepigmc/wealthofnations/GamePanel.java
@@ -0,0 +1,26 @@
+package tepigmc.wealthofnations;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+  private static final long serialVersionUID = -688258623776074646L;
+  int x = 0, y = 0;
+
+  void moveBall() {
+    x++;
+    y++;
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    super.paint(g);
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+    g2d.fillOval(x, y, 30, 30);
+  }
+}
